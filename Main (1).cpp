19feab1e4d58c63d6c89ee79de5d59e33d9d4db7
vaishnavi-pote-2106 <vaishnavipote713@gmail.com>
2024-09-15 19:07:04 +0530

    Add files via upload

--- Main (1).cpp
+++ Main (1).cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main()
 {
-  for (int i=1;i<=5;++i)
-  {
-    cout<<i<<"";
-    
-  }
-  cout<<endl;
-return 0;
+  int a=2, b=4;
+  cout<<"a&b is "<<(a&b)<<endl;
+  cout<<"a/b is "<<(a/b)<<endl;
+  cout<<"a^b is "<<(a^b)<<endl;
+  cout<<"a<<b is "<<(a<<b)<<endl;
+  cout<<"a>>b is "<<(a>>b)<<endl;
+  cout<<"-(a) is "<<-(a)<<endl;
+    return 0;
 }
